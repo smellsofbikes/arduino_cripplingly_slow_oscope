8eed9fe8cb32aa6e43c55ddfea8d113e7fc14a40
John <smellsofbikes@users.noreply.github.com>
2019-09-26 13:46:23 -0600

    I mistakenly deleted some includes...

--- tft_moving_voltage_graph.ino
+++ tft_moving_voltage_graph.ino
@@ -4,6 +4,11 @@
 
 #include <Adafruit_GFX.h>    // Core graphics library
 #include <Adafruit_TFTLCD.h> // Hardware-specific library
+#define LCD_CS A3 // Chip Select goes to Analog 3
+#define LCD_CD A2 // Command/Data goes to Analog 2
+#define LCD_WR A1 // LCD Write goes to Analog 1
+#define LCD_RD A0 // LCD Read goes to Analog 0
+#define LCD_RESET A4 // Can alternately just connect to Arduino's reset pin
 
 // Assign human-readable names to some common 16-bit color values:
 #define	BLACK   0x0000
